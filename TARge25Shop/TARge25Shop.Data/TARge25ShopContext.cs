1974dd2b7e3022af1dfd9130f52afcdef9de6606
Katri Lotamõis <katrilotamois@outlook.com>
2026-09-07 16:55:04 +0300

    create dbset for db context

--- TARge25Shop/TARge25Shop.Data/TARge25ShopContext.cs
+++ TARge25Shop/TARge25Shop.Data/TARge25ShopContext.cs
@@ -1,4 +1,7 @@
+using TARge25Shop.Core.Domain;
+
 namespace TARge25Shop.Data;
+using TARge25Shop.Core.Domain;
 
 using Microsoft.EntityFrameworkCore;
 
@@ -7,4 +10,10 @@ public class TARge25ShopContext : DbContext
     public TARge25ShopContext(DbContextOptions<TARge25ShopContext> options) : base(options)
     {}
     
+    public DbSet<Spaceship> Spaceships { get; set; }
+    
+    // protected override void OnModelCreating(ModelBuilder modelBuilder)
+    // {
+    //     modelBuilder.Entity<Spaceship>()
+    // }
 }
