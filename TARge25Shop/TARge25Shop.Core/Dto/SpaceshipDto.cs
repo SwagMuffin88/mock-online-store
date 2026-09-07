15602df8c889388d12cb9b6eade5468445e98dd4
Katri Lotamõis <katrilotamois@outlook.com>
2026-09-07 16:24:50 +0300

    add missing project references

---
+++ TARge25Shop/TARge25Shop.Core/Dto/SpaceshipDto.cs
@@ -0,0 +1,13 @@
+namespace TARge25Shop.Core.Dto;
+
+public class SpaceshipDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string ShipType { get; set; } =  string.Empty;
+    public int MaxCrewSize { get; set; }
+    public int EnginePower { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
