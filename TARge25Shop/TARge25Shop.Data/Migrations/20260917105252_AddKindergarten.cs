aa1ac08cf15e3b2adf7499c43ea1c7f0c9990486
Katri Lotamõis <katrilotamois@outlook.com>
2026-09-17 14:00:59 +0300

    add migrations and inject service into project build

---
+++ TARge25Shop/TARge25Shop.Data/Migrations/20260917105252_AddKindergarten.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TARge25Shop.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddKindergarten : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Kindergartens",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    GroupName = table.Column<string>(type: "TEXT", nullable: false),
+                    ChildrenCount = table.Column<int>(type: "INTEGER", nullable: false),
+                    KindergartenName = table.Column<string>(type: "TEXT", nullable: false),
+                    TeacherName = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kindergartens", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Kindergartens");
+        }
+    }
+}
